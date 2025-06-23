b14645944dc507c134d84ee2b46abb303866f804
𓂺 <ac7x7x@gmail.com>
2025-06-23 19:47:23 +0800

    新增 getUsersCount 函數以支持根據日期範圍查詢用戶數量，並新增 getTotalUsersCounts 函數以批量獲取多個日期的用戶數量；更新 useUsersCount 和 useUsersCountByTotalDays 以整合新邏輯。

--- lib/firestore/user/read_count.jsx
+++ lib/firestore/user/read_count.jsx
@@ -7,18 +7,63 @@ import {
   count,
   getAggregateFromServer,
   getCountFromServer,
+  query,
+  where,
 } from "firebase/firestore";
 import useSWR from "swr";
 
-export const getUsersCount = async () => {
+export const getUsersCount = async ({ date }) => {
   const ref = collection(db, `users`);
-  const data = await getCountFromServer(ref);
-  return data.data().count;
+  let q = query(ref);
+
+  if (date) {
+    const fromDate = new Date(date);
+    fromDate.setHours(0, 0, 0, 0);
+    const toDate = new Date(date);
+    toDate.setHours(24, 0, 0, 0);
+    q = query(
+      q,
+      where("timestampCreate", ">=", fromDate),
+      where("timestampCreate", "<=", toDate)
+    );
+  }
+  const data = await getAggregateFromServer(q, {
+    totalUsers: count(),
+  });
+  if (date) {
+    return {
+      date: `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`,
+      data: data.data(),
+    };
+  }
+  return data.data().totalUsers;
+};
+
+const getTotalUsersCounts = async (dates) => {
+  let promisesList = [];
+  for (let i = 0; i < dates?.length; i++) {
+    const date = dates[i];
+    promisesList.push(getUsersCount({ date: date }));
+  }
+  const list = await Promise.all(promisesList);
+  return list;
 };
 
 export function useUsersCount() {
   const { data, error, isLoading } = useSWR("users_count", (key) =>
-    getUsersCount()
+    getUsersCount({ date: null })
+  );
+  if (error) {
+    console.log(error?.message);
+  }
+  return { data, error, isLoading };
+}
+
+export function useUsersCountByTotalDays({ dates }) {
+  const { data, error, isLoading } = useSWR(
+    ["users_count", dates],
+    ([key, dates]) =>
+      getTotalUsersCounts(dates?.sort((a, b) => a?.getTime() - b?.getTime()))
   );
   if (error) {
     console.log(error?.message);
